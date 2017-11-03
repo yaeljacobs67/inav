dbe10a0107cec4700aa2f7511536629075539ad5
Alberto García Hierro <alberto@garciahierro.com>
2017-11-03 18:10:26 +0000

    Move wind estimator to flight/, call it only on GPS updates
    - Move navigation_wind_estimator.{c,h} to flight/wind_estimator.{c,h}
    - Rename gpsThread() to gpsUpdate(), make it return a bool which is
    true iff new GPS data was received.
    - When the GPS returns new data, call into the wind estimator to
    update itself.
    - Make sure wind estimator can be disabled at compile time via
    the USE_WIND_ESTIMATOR #ifdef
    - Disable OSD wind indicators when wind estimator support is not
    compiled in.
    - Don't check for both GPS and USE_WIND_ESTIMATOR in ifdefs, instead
    fail compilation with an error in USE_WIND_ESTIMATOR is defined
    without defining GPS.

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -70,6 +70,7 @@
 
 #include "flight/imu.h"
 #include "flight/pid.h"
+#include "flight/wind_estimator.h"
 
 #include "navigation/navigation.h"
 
@@ -356,6 +357,7 @@ static void osdFormatVelocityStr(char* buff, int32_t vel)
  * is null terminated.
  * @param ws Raw wind speed in cm/s
  */
+#ifdef USE_WIND_ESTIMATOR
 static void osdFormatWindSpeedStr(char *buff, int32_t ws, bool isValid)
 {
     int32_t centivalue;
@@ -380,6 +382,7 @@ static void osdFormatWindSpeedStr(char *buff, int32_t ws, bool isValid)
     buff[3] = suffix;
     buff[4] = '\0';
 }
+#endif
 
 /**
 * Converts altitude into a string based on the current unit system
@@ -1549,6 +1552,7 @@ static bool osdDrawSingleElement(uint8_t item)
         }
 
     case OSD_WIND_SPEED_HORIZONTAL:
+#ifdef USE_WIND_ESTIMATOR
         {
             bool valid = isEstimatedWindSpeedValid();
             float horizontalWindSpeed;
@@ -1574,8 +1578,12 @@ static bool osdDrawSingleElement(uint8_t item)
             osdFormatWindSpeedStr(buff + 2, horizontalWindSpeed, valid);
             break;
         }
+#else
+        return false;
+#endif
 
     case OSD_WIND_SPEED_VERTICAL:
+#ifdef USE_WIND_ESTIMATOR
         {
             buff[0] = SYM_WIND_VERTICAL;
             buff[1] = SYM_BLANK;
@@ -1595,6 +1603,9 @@ static bool osdDrawSingleElement(uint8_t item)
             osdFormatWindSpeedStr(buff + 2, verticalWindSpeed, valid);
             break;
         }
+#else
+        return false;
+#endif
 
     default:
         return false;
